961189075a756c602c98f5f9d76addb14fdb1675
Nabi Isakhanov <nabi.isakhanov@dataart.com>
2020-12-22 19:53:36 +0300

    Add EN and RU translations, copy EN ones to UA and AM

---
+++ src/am/new_year/task3/solution.py
@@ -0,0 +1,14 @@
+import raccoon
+
+raccoon.go_left(3)
+raccoon.go_up(3)
+raccoon.go_right(2)
+raccoon.go_left(1)
+raccoon.go_up(3)
+raccoon.place_turret()
+raccoon.go_right(1)
+raccoon.wait(20)
+raccoon.go_up(1)
+raccoon.go_right(6)
+raccoon.go_down(7)
+raccoon.go_left(3)
